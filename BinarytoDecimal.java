8e6ae31cfddb32bb4b2bd4efde5c39f274489a0a
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2019-11-27 15:43:59 +0530

    Create BinarytoDecimal.java

---
+++ BinarytoDecimal.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+/**
+ * @author LENOVO YOGA
+ *
+ */
+public class BinarytoDecimal {
+
+	/**
+	 * @param args
+	 */
+	public static int binarytod(long n)
+	{
+		int p=0;
+		int rem;
+		int sum=0;
+		while(n!=0)
+		{
+			rem=(int)n%10;
+			sum+=rem*Math.pow(2,p);
+			p++;
+			n=n/10;
+			
+		}
+		return sum;
+	}
+	public static void main(String[] args)
+	{
+		long n;
+	System.out.println("enter a number");	
+	Scanner sc=new Scanner(System.in);
+	n=sc.nextLong();
+		System.out.println("roman equivalent :"+ binarytod(n));
+	}
+	
+	
+	}
