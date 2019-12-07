ab537391f2aec6924a471526ee493b88fc09c2bb
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2019-12-07 12:28:17 +0530

    Create Uppertolower.java

---
+++ Uppertolower.java
@@ -0,0 +1,21 @@
+
+		
+import java.util.Scanner;
+
+public class Uppertolower
+{
+    public static void main(String[] input)
+    {
+        char ch;
+        int temp;
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter a Character in Uppercase : ");
+        ch = scan.next().charAt(0);
+		
+        temp = (int) ch;
+        temp = temp + 32;
+        ch = (char) temp;
+		
+        System.out.print("Equivalent Character in Lowercase = " + ch);
+   
