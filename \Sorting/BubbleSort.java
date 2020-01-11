e90bff50a7b07942dc0402d0ec46f27871dc046d
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2020-01-11 23:40:13 +0530

    Add bubblesort

---
+++ \Sorting/BubbleSort.java
@@ -0,0 +1,44 @@
+/*Sorting is nothing but arranging the data in a logical order. It might be in acsending or descending order. 
+For alpanumeric values it can be arranged in dictionary order.
+*********BUBBLE SORT*********
+Comparision based algorithm i.e it compares he adjacent value of its element and swaps them if they are in wrong order.
+*/
+
+import java.util.Scanner;
+
+class BubbleSort {
+	public static void main(String args[])
+	{
+		int n,temp;
+		System.out.println("enter the size of the array");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<arr.length;i++)
+		{
+            arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			int flag=0;
+			for(int j=0;j<arr.length-1-i;j++)
+			{
+				if(arr[j]<arr[j+1])
+				{
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+				flag=1;
+			}
+		}
+			if(flag==0)
+				break;
+	}
+		for(int i=0;i<arr.length;i++)
+		System.out.println(arr[i]+"");
+}
+}
+    	
+      
+      
+    	
