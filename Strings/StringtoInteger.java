a38fc7a36ce49f77455d03525fda418b7e2e495c
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2020-02-17 15:32:45 +0530

    Create StringtoInteger.java

---
+++ Strings/StringtoInteger.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+String str="65";
+int result=Integer.parseInt(str);
+System.out.println(result);
+  }
+}
