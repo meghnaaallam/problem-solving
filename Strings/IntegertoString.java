4ee59aeed5d71bd407e9b24f39ecaf75f936f569
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2020-02-17 15:30:21 +0530

    Create IntegertoString.java

---
+++ Strings/IntegertoString.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int str=35;
+String result=Integer.toString(str);
+System.out.println(result);
+  }
+}
