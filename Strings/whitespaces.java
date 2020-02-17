95c48cc10ef6be841d9b762bd789d9e853217d08
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2020-02-17 13:11:13 +0530

    added additional changes

--- Strings/whitespaces.java
+++ Strings/whitespaces.java
@@ -19,3 +19,38 @@ class Main {
    }
   }
 }
+
+
+******METHOD 2*****
+class Main {
+  public static void main(String[] args)
+  {
+  int i,j;
+   {
+  Scanner sc=new Scanner(System.in);
+  String str=sc.nextLine();
+  char ch[]=str.toCharArray();
+  String str1="";
+  for(i=0;i<ch.length;i++)
+  {
+     if(ch[i]!=' ')
+     {
+       str1=str1+ch[i];
+     }
+  }
+  System.out.println(str1);
+   }
+  }
+}
+
+
+*********METHOD 3*******
+class Main {
+  public static void main(String[] args)
+  {
+     Scanner sc=new Scanner(System.in);
+  String str=sc.nextLine();
+  str.replace(regex:" ",replacement:"");
+  System.out.println(str);
+  }
+  }
