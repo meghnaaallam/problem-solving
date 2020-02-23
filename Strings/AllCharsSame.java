90bce227f9ca2e3088b7aad93c3344b2547c4b69
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2020-02-23 18:01:16 +0530

    Create AllCharsSame.java

---
+++ Strings/AllCharsSame.java
@@ -0,0 +1,28 @@
+// Java program to find whether the String 
+// has all same characters or not. 
+import java.io.*; 
+
+public class Stringornot{ 
+
+static boolean allCharactersSame(String s) 
+{ 
+	int n = s.length(); 
+	for (int i = 1; i < n; i++) 
+		if (s.charAt(i) != s.charAt(0)) 
+			return false; 
+		
+	return true; 
+} 
+
+// Driver code 
+	static public void main (String[] args){ 
+		String s = "aaa"; 
+	if (allCharactersSame(s)) 
+		System.out.println("Yes"); 
+	else
+		System.out.println("No"); 
+		
+	} 
+} 
+
+
