bebe10994dd6103472ab0f1a561ffa162bfb46e1
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2020-03-05 10:25:46 +0530

    Update Reverseofwords.java

--- Strings/Reverseofwords.java
+++ Strings/Reverseofwords.java
@@ -11,10 +11,10 @@ class Main {
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
 		String[] str2=str.split(" ");
-		for(int i=str2.length()-1;i>=0;i--)
+		for(int i=str2.length-1;i>=0;i--)
 		{
 			
-	System.out.print(str2)
+	System.out.print(str2[i]+" ");
 	}
 	}		
 }
