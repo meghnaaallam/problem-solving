979bc8863b002103ccc98aa9281338ca9dbb58b5
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2020-03-05 17:42:37 +0530

    Update AllCharsSame.java

--- Strings/AllCharsSame.java
+++ Strings/AllCharsSame.java
@@ -25,36 +25,3 @@ static boolean allCharactersSame(String s)
 	} 
 } 
 
-/*method 2*/
-import java.util.HashMap;
-import java.util.*;
-class Main {
-  public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    String str=sc.next();
-    int count=0;
-    HashMap<Character,Integer> hm=new HashMap<Character,Integer>();
-for(int i=0;i<str.length();i++)
-{
-  char current=str.charAt(i);
-  if(hm.containsKey(current))
-  {
-    count=count+1;
-  hm.put(current,count);
-  }
-  else
-  {
-  hm.put(current,1);
-  count=1;
-}
-  }
-for(int i=0;i<str.length();i++)
-{
-  char x=str.charAt(i);
-  if(hm.get(x)==1) {
-  System.out.println("first repeated character: "+x);
-  break;
-}
-}
-}
-}
