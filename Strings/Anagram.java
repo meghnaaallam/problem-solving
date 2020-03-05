6d965fca127bc849a1c9c8eb13f3696d00cc1a17
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2020-03-05 12:01:39 +0530

    Update Anagram.java

--- Strings/Anagram.java
+++ Strings/Anagram.java
@@ -5,7 +5,7 @@ output:anagram
 
 all the charas should be present,then anagram
 */
-
+/*Method 1*/
 
 import java.util.*;
 class Main {
@@ -27,3 +27,40 @@ class Main {
     return Arrays.equals(ch1,ch2);
   }
 }
+/* method 2*/
+
+
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+   Scanner sc=new Scanner(System.in);
+   String str1=sc.nextLine();
+   String str2=sc.nextLine();
+   boolean isAnagram=true;
+   int al[]=new int[256];
+   int bl[]=new int[256];
+  for(char c: str1.toCharArray()) 
+ {
+     int index=(int)c;
+     al[index]++;
+ }
+for(char c: str2.toCharArray()) 
+ {
+     int index=(int)c;
+     bl[index]++;
+ }
+ for(int i=0;i<256;i++)
+ {
+   if(al[i]!=bl[i])
+   {
+     isAnagram=true;
+     break;
+   }
+ }
+ if(isAnagram)
+ System.out.print("anagram");
+else
+System.out.print("not anagram");
+ }
+  }
+  
