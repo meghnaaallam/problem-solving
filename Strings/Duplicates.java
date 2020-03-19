ab8701ad19a7eeb973bc7983716aa9187802492e
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2020-03-19 15:18:10 +0530

    Update Duplicates.java

--- Strings/Duplicates.java
+++ Strings/Duplicates.java
@@ -25,3 +25,45 @@ class Main {
   }
 }
 }
+
+*****************
+/*for words
+input: cat batman latt matter cat matter cat
+output: cat matter*/
+
+import java.util.*;
+  class Main {
+     public static void main (String[] args) {
+          /* code */
+          
+          Scanner sc=new Scanner(System.in);
+          String str=sc.nextLine();
+          expandIt(str);
+     }
+     static void expandIt(String str) {
+         int count=0;
+         String[] ch=str.split(" ");
+         HashMap<String,Integer> hm=new HashMap<>();
+         for(int i=0;i<ch.length;i++)
+         {
+             String current=ch[i];
+             if(hm.containsKey(current))
+             {
+                 count=count+1;
+                 hm.put(current,count);
+             }
+             else {
+                 hm.put(current,1);
+                 count=1;
+             }
+         }
+           Set<String> keys=hm.keySet();
+  for(String cha:keys)
+  {
+    if(hm.get(cha)>1)
+       System.out.print(cha+" ");
+     }
+  }
+  }
+
+
