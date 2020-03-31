6dc0440ece6d0937fa4d6b2398ba654b1cfa7f41
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2020-03-31 13:12:59 +0530

    Create FirstRepeatedElement.java

---
+++ Arrays/FirstRepeatedElement.java
@@ -0,0 +1,28 @@
+*/input: 1 22 1 34 55
+output: 1
+*/
+
+
+
+
+
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("enter the sixe of the array");
+    Scanner sc=new Scanner(System.in);
+    int size=sc.nextInt();
+    int arr[]=new int[size];
+    for(int i=0;i<arr.length;i++) {
+      arr[i]=sc.nextInt();
+    }
+    for(int i=0;i<arr.length;i++) {
+      for(int j=0;j<arr.length;j++) {
+        if(arr[i]==arr[j] && i!=j) {
+          System.out.println("first repeated element is:"+ arr[j]);
+        }
+        break;
+      }
+    }
+  }
+}
