cf676a414ac2ecdd929eb36a717c04fff6d8a190
meghnareddy1999 <53735437+meghnareddy1999@users.noreply.github.com>
2020-03-31 13:30:35 +0530

    Update FirstRepeatedElement.java

--- Arrays/FirstRepeatedElement.java
+++ Arrays/FirstRepeatedElement.java
@@ -24,3 +24,36 @@ class Main {
     }
   }
 }
+
+
+//method 2//
+
+
+
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+  System.out.print("enter the size of the array:");
+  Scanner sc=new Scanner(System.in);
+  int size=sc.nextInt();
+  int arr[]=new int[size];
+  for(int i=0;i<arr.length;i++) {
+    arr[i]=sc.nextInt();
+  }
+  firstRepeatingElement(arr);
+}
+static void firstRepeatingElement(int arr[]) {
+  int min=0;
+  Set<Integer> hs=new HashSet<Integer>();
+  for(int i=0;i<arr.length;i++) {
+    if(hs.contains(arr[i]))
+    min=i;
+    else
+    hs.add(arr[i]);
+  }
+  if(min!=0) 
+  System.out.print("the first repeating element:" +arr[min]);
+  else
+  System.out.print("no repeating elements");
+}
+}
